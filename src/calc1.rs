4b3cd6bcef75763d75a4d41e80f99fd95e4a1be7
Huggo Santos Oliveira <huggooliveira@MacBook-Air-de-Huggo.local>
2025-08-15 16:48:37 -0300

    ajustes name lib

--- src/calc1.rs
+++ src/calc1.rs
@@ -3,6 +3,11 @@ pub fn add(a: u32, b: u32) -> u32 {
     a + b
 }
 
+// func para subtrair, se o primeiro numero for menor que o segundo, retorna 0
 pub fn sub(a: u32, b: u32) -> u32 {
-    if a < b { 0 } else { a - b }
+    if a < b {
+        0
+    } else {
+        a - b
+    }
 }
